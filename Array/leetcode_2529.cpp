076f04eae50a5cdb96b9c5ade32355051720801f
SagoRohit <ch.sagor27@gmail.com>
2025-03-12 10:52:12 +0600

    leetcode_2529.cpp

---
+++ Array/leetcode_2529.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+        int maximumCount(vector<int>& nums) {
+            int neg=0, pos=0;
+        for(int num: nums){
+            if(num<0)
+                neg++;
+            else if(num>0)
+                pos++;
+            else
+                continue;
+        }
+        return (neg>pos)? neg:pos;
+        }
+    };
