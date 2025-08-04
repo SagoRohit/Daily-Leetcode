74dbae71d14bceab937fa3053ba2119971079983
Sagor Chanda <ch.sagor27@gmail.com>
2025-08-04 12:22:09 +0600

    leetcode-118

---
+++ Array/leetcode-118.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        int n = numRows;
+        for(int i=0; i<n; i++)
+        {
+            vector<int> v(i+1,1);
+            for(int j=1; j<i; j++)
+            {
+                v[j]= result[i-1][j] + result [i-1][j-1];
+            }
+            result.push_back(v);
+        }
+        return result;
+    }
+};
