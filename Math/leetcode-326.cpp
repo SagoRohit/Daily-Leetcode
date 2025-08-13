7fe2795f745261b2475cb3232090e819929a2d1b
Sagor Chanda <ch.sagor27@gmail.com>
2025-08-13 11:39:25 +0600

    leet-326 and new approach

--- Math/leetcode-326.cpp
+++ Math/leetcode-326.cpp
@@ -15,3 +15,11 @@ public:
         return true;
     }
 };
+
+// here 1162261467 is the largest power-3th number of int which is 3^19. so if this is divisible by n, then obviously n is a power of 3.
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         return n>0 && 1162261467%n==0;
+//     }
+// };
