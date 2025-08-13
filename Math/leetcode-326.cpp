60f687c7676887b865108d857ed7f9539e6b90fb
Sagor Chanda <ch.sagor27@gmail.com>
2025-08-13 11:16:50 +0600

    leet-326

---
+++ Math/leetcode-326.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0)
+            return false;
+        while(n!=1){
+            if(n%3!=0)
+                return true;
+            else
+                n/=3;
+        }
+        return true;
+    }
+};
