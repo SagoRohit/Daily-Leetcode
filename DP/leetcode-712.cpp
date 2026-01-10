e86940319ff08fb1484fe14e828ec0af1f16a284
Sagor Chanda <ch.sagor27@gmail.com>
2026-01-11 00:53:32 +0600

    leetcode-712

---
+++ DP/leetcode-712.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string.h>
+using namespace std;
+class Solution {
+public:
+    int m,n;
+    int t[1001][1001];
+    int solve(int i, int j, string &s1, string &s2){
+        if(i>=m && j>=n)
+            return 0;
+        if(t[i][j] != -1)
+            return t[i][j];
+        if(i>=m)
+            return t[i][j]= s2[j] + solve(i, j+1, s1, s2);
+        else if(j>=n)
+            return t[i][j]=s1[i] + solve(i+1, j, s1, s2);
+        if(s1[i]==s2[j])
+            return t[i][j]=solve(i+1, j+1, s1, s2);
+        int deletes1 = s1[i] + solve(i+1, j, s1,s2);
+        int deletes2 = s2[j] + solve(i,j+1,s1,s2);
+
+        return t[i][j]=min(deletes1, deletes2);
+    }
+    int minimumDeleteSum(string s1, string s2) {
+        m = s1.size();
+        n = s2.size();
+        memset(t, -1, sizeof(t));
+        return solve(0, 0, s1, s2);
+    }
+};
