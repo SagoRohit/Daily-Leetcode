21f5ead86afb286705af13ad5a58eba4f3c79ae4
Sagor Chanda <ch.sagor27@gmail.com>
2026-02-19 11:09:41 +0600

    leetcode-55

---
+++ Greedy/leetcode-55.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int furthest=0;
+        int n = nums.size();
+        for(int i=0; i<n; i++)
+        {
+            if(i>furthest)
+                return false;
+            furthest= max(furthest, i+nums[i]);
+            if(furthest>=n-1)
+                return true;
+        }
+        return false;
+    }
+};
+int main(){
+
+    return 0;
+}
