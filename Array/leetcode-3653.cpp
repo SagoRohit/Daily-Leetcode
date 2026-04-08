01a8df1d37c12ae702addb53d4dc12e4cbeb45a2
Sagor Chanda <ch.sagor27@gmail.com>
2026-04-09 02:16:36 +0600

    leetcode-3653.cpp

---
+++ Array/leetcode-3653.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int M = 1e9+7;
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        for(auto q: queries){
+            int l = q[0];
+            int r = q[1];
+            int k = q[2];
+            int v = q[3];
+            while(l<=r){
+                nums[l] = (1LL * nums[l] * v) % M;
+                l+=k;
+            }
+            int result = 0;
+            for(int i=0; i<nums.size(); i++){
+                result ^= nums[i];
+            }
+            return result;
+        }
+    }
+};
