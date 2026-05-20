07f4ed79b81cde7c86231faf43f8f4be1df0d9c7
Sagor Chanda <ch.sagor27@gmail.com>
2026-05-20 11:16:23 +0600

    practicing prev questions for warmup

--- Array/leetcode-1.cpp
+++ Array/leetcode-1.cpp
@@ -1,29 +1,21 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 #include<unordered_map>
 using namespace std;
-class Solution
-{
+class Solution {
 public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-        // unordered_map<int, int> map;
-        // int n = nums.size();
-        // for(int i=0; i<n; i++){
-        //     int compliment = target - nums[i];
-        //     if(map.count(compliment)>0){
-        //         return {map[compliment], i};
-        //     }
-        //     map[nums[i]]= i;
-        // }
-        // return {};
-        int n = nums.size();
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j]==target)
-                    return {i,j};
-            }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            int complement = target - nums[i];
+            if(mp.count(complement))
+                return {i, mp[complement]};
+            mp[nums[i]] = i;
         }
         return {};
     }
 };
+int main(){
+
+    return 0;
+}
