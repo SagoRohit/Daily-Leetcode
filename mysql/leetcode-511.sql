ebb0ca39dd895bf6a21386251c1cb5e833a7943c
Sagor Chanda <ch.sagor27@gmail.com>
2026-05-25 13:01:59 +0600

    leetcode 511 first login date finding

--- mysql/leetcode-511.sql
+++ mysql/leetcode-511.sql
@@ -1,5 +1,6 @@
-select player_id, 
-min(event_date) as first_login
-from Activity 
-group by player_id
-;
+select player_id, min(event_date) as first_login
+from activity
+GROUP BY player_id
+
+-- here we group the activity of each player.
+-- then we tried to find the min date to find the first login
