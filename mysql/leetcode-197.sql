56264640c8446ffea30abcc5c8504fe5cc8bd065
Sagor Chanda <ch.sagor27@gmail.com>
2026-05-25 12:49:47 +0600

    leetcode-197 using datediff function to find the immediate 2 dates and then comparison to check if the temp is greater or not

--- mysql/leetcode-197.sql
+++ mysql/leetcode-197.sql
@@ -1,5 +1,10 @@
-select today.id 
-from Weather today
-cross join Weather yesterday
-where datediff(today.recordDate, yesterday.recordDate)=1
-and today.temperature > yesterday.temperatur;
+select w1.id
+from weather w1, weather w2
+where DATEDIFF(w1.recorddate, w2.recorddate)=1 and 
+w1.temperature > w2.temperature
+
+-- comma seperated makes the table become join.
+-- and the both table will be cross joined to each other.
+-- and then datediff will delete w2 from w1. thus w1 temp
+-- should be greater than w2. Means, next day temp is greater
+-- than the previous day temp.
