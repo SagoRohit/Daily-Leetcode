387e78dd92a9ff13a64d340bb45075bba434e141
Sagor Chanda <ch.sagor27@gmail.com>
2026-05-25 11:22:38 +0600

    leetcode-181 using left outer join

--- mysql/leetcode-181.sql
+++ mysql/leetcode-181.sql
@@ -3,4 +3,9 @@ select e.name as Employee
 from employee e 
 where e.salary > ( select m.salary 
                     from employee m
-                    where e.managerid = m.id)
+                    where e.managerid = m.id);
+
+SELECT e.name as Employee
+from employee e left join employee m
+on e.managerid = m.id
+where e.salary > m.salary;
