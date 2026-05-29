6f0bc9b9ab6ab7c8857b4e226bc4f291d243bc54
Sagor Chanda <ch.sagor27@gmail.com>
2026-05-29 13:29:38 +0600

    leetcode 80

---
+++ Array/leetcode-80.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int n: nums){
+            if(i<2 || n!=nums[i-2]){
+                nums[i++]=n;
+            }
+        }
+        return i;
+    }
+};
+int main(){
+    Solution sol;
+    vector<int> vec{};
+    cout<<sol.removeDuplicates(vec);
+    return 0;
+}
