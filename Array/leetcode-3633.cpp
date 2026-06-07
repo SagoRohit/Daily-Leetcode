ec0173e9e376fd8ed1353c81a6ecac9ff0df3822
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-08 00:33:10 +0600

    leetcode-3633.cpp (optimized version)

--- Array/leetcode-3633.cpp
+++ Array/leetcode-3633.cpp
@@ -37,3 +37,24 @@ public:
         return finaltime;
     }
 };
+
+class Solution2 {
+public:
+    int finishtime(vector<int> &start1, vector<int>& duration1, vector<int>& start2, vector<int>& duration2){
+        int finish1 = INT_MAX;
+        int finish2 = INT_MAX;
+        for(int i=0; i<start1.size(); i++){
+            finish1 = min(finish1, start1[i]+duration1[i]);
+        }
+        for(int i=0; i<start2.size(); i++){
+            finish2 = min(finish2, max(finish1, start2[i])+duration2[i]);
+        }
+        return finish2;
+    }
+    int earliestFinishTime2(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int result1 = finishtime(landStartTime, landDuration, waterStartTime, waterDuration); // start with land ride
+        int result2 = finishtime(waterStartTime, waterDuration, landStartTime, landDuration); // start with water ride
+
+        return min(result1, result2);
+    }
+};
