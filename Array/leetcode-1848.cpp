3dae0f5ee45a2d286efc6b01b0f26d8731b0f7d1
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-08 00:43:42 +0600

    leetcode-1848.cpp

---
+++ Array/leetcode-1848.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int mini = INT_MAX;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==target)
+            {
+                mini = min(mini, abs(i-start));
+            }
+        }
+        return mini;
+    }
+};
