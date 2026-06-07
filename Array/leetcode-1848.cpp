2f6eae17d9820ba2d8e478d335827a4683053176
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-08 00:49:49 +0600

    leetcode-1848.cpp (better version)

--- Array/leetcode-1848.cpp
+++ Array/leetcode-1848.cpp
@@ -14,3 +14,20 @@ public:
         return mini;
     }
 };
+class Solution1 {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        if(nums[start]==target)
+            return 0;
+        int ind = 1;
+        while(true)
+        {
+            if(start-ind>=0 and nums[start-ind]==target)
+                return ind;
+            if(start+ind<nums.size() and nums[start+ind]==target)
+                return ind;
+            ind++;
+        }
+        return -1;
+    }
+};
