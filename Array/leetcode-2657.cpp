219cb4a2b800e2c0c19cba8c82081f049be9d52e
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-10 00:50:53 +0600

    leetcode-2657 (best approach - using 1 map only)

--- Array/leetcode-2657.cpp
+++ Array/leetcode-2657.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<unordered_set>
+#include<unordered_map>
 using namespace std;
 class Solution {
 public:
@@ -20,6 +21,24 @@ public:
             if(a[i]==b[i])
                 tempcount = 1;
             count += tempcount;
+            result.push_back(count);
+        }
+        return result;
+    }
+};
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& a, vector<int>& b) {
+        vector<int> result;
+        int count=0;
+        unordered_map<int, int> mp;
+        for(int i=0; i<a.size(); i++){
+            mp[a[i]]++;
+            if(mp[a[i]]==2)
+                count++;
+            mp[b[i]]++;
+            if(mp[b[i]]==2)
+                count++;
             result.push_back(count);
         }
         return result;
