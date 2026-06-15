34a468ad9245c683c96a413a18bf463b808d8811
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-15 23:59:02 +0600

    leetcode-3838.cpp

--- String/leetcode-3838.cpp
+++ String/leetcode-3838.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 class Solution {
 public:
+    // using the string mapping
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
         string result="";
         for(string s: words){
             int sum = 0;
             for(char c: s){
-                sum += weights[c-'a'];
+                sum += weights[c-'a']; // calculate sum of weights
             }
             sum %= 26;
             result += ('a'+ abs(sum-25));
