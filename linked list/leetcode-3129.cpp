34a7709dfd7161123230b8c2e943170d819f403e
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-17 19:07:14 +0600

    leetcode-3120 (appr-2): using stack

--- linked list/leetcode-3129.cpp
+++ linked list/leetcode-3129.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
 struct ListNode
 {
@@ -21,6 +22,27 @@ public:
         int n = value.size();
         for(int i=0; i<n; i++){
             result = max(result, value[i]+value[n-i-1]);
+        }
+        return result;
+    }
+};
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        stack<int> st;
+        ListNode* temp = head;
+        while(head!= nullptr){
+            st.push(head->val);
+            head = head->next;
+        }
+        int n = st.size();
+        int result=0;
+        int i = 0;
+        while(i<n/2){
+            result = max(result, temp->val+st.top());
+            temp = temp->next;
+            st.pop();
+            i++;
         }
         return result;
     }
