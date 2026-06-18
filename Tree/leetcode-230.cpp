4265db763871a14ba69d2a3c98f363c4a14d1672
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-18 21:18:57 +0600

    leetcode-230 (using vector)

---
+++ Tree/leetcode-230.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    void inorder(TreeNode* node, vector<int>& res){
+        if(node==NULL)
+            return;
+        inorder(node->left, res);
+        res.push_back(node->val);
+        inorder(node->right, res);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> result;
+        inorder(root, result);
+        return result[k-1];
+    }
+};
