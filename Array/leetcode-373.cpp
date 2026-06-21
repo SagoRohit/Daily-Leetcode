2e3b7d48fb8212b4359713e53cc43cc90e7eb7dc
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-21 21:41:20 +0600

    leetcode-373 now the optimal solution

--- Array/leetcode-373.cpp
+++ Array/leetcode-373.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<set>
 #include<queue>
 using namespace std;
 class Solution {
@@ -27,6 +28,42 @@ public:
             int i = temp.second.first;
             int j = temp.second.second;
             result.push_back({nums1[i], nums2[j]});
+        }
+        return result;
+    }
+};
+
+
+class Solution {
+public:
+    typedef pair<int, pair<int, int>> p;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int m = nums1.size();
+        int n = nums2.size();
+        priority_queue<p, vector<p>, greater<p>> pq;
+        set<pair<int, int>> visited;
+        vector<vector<int>> result;
+
+        int sum = nums1[0] + nums2[0];
+        pq.push({sum, {0,0}});
+        visited.insert({0,0});
+        
+        while(k-- and !pq.empty()) {
+            auto temp = pq.top();
+            pq.pop();
+            int i = temp.second.first;
+            int j = temp.second.second;
+            result.push_back({nums1[i], nums2[j]});
+            // now we explore (i+1, j)
+            if(i+1 < m and visited.find({i+1,j})==visited.end()){
+                pq.push({nums1[i+1]+nums2[j], {i+1, j}});
+                visited.insert({i+1,j});
+            }
+            // now we explore (i, j+1)
+            if(j+1 < n and visited.find({i,j+1})==visited.end()){
+                pq.push({nums1[i]+nums2[j+1], {i, j+1}});
+                visited.insert({i,j+1});
+            }
         }
         return result;
     }
