79a12fc2e85701e67c7b33661a1f8682dfdf2d70
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-27 21:38:56 +0600

    leetcode-201

---
+++ bit/leetcode-201.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+// here the core idea is to find the prefix of left and right. and then pad i amount of 0 at the end of common prefix.
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int i=0;
+        while(left != right) {
+            left >>= 1; // right shifting to find the common prefix
+            right >>= 1;
+            i++; // shift count
+        }
+        left <<= i; // now pad zero.
+        return left; 
+    }
+};
