1c08f655ce6c73074fb0c2358908f032aac835e4
Sagor Chanda <ch.sagor27@gmail.com>
2026-08-12 22:08:02 +0600

    leetcode-1321

---
+++ mysql/leetcode-1321.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+select visited_on,
+    (
+        select sum(amount)
+        from customer
+        where visited_on BETWEEN date_sub(c.visited_on, interval 6 day)
+        and c.visited_on
+    ) as amount,
+    round(
+        (
+        select sum(amount)
+        from customer
+        where visited_on BETWEEN date_sub(c.visited_on, interval 6 day)
+        and c.visited_on
+        )/7
+        ,2) as average_amount
+from customer c
+where visited_on >= (
+    select date_add(min(visited_on), interval 6 day)
+    from customer
+)
+group by visited_on
+order by visited_on
