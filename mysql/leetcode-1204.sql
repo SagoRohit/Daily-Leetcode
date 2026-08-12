50624ddf5875a7116bb77b7983a101a733eb6c3a
Sagor Chanda <ch.sagor27@gmail.com>
2026-08-12 19:28:54 +0600

    leetcode-1204

--- mysql/leetcode-1204.sql
+++ mysql/leetcode-1204.sql
@@ -5,3 +5,13 @@ group by q1.turn
 having sum(q2.weight) <= 1000
 order by sum(q2.weight) desc
 limit 1
+
+# 2nd approach using window function over() and cte
+with temp as (
+    select *, sum(weight) over(order by turn) as running_weight
+    from queue
+)
+select person_name
+from temp 
+where running_weight <= 1000
+order by running_weight desc limit 1
