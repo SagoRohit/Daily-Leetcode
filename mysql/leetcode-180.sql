d81dcf970aeb205c793d7fde6bb4c846c53f5ff3
Sagor Chanda <ch.sagor27@gmail.com>
2026-08-12 10:32:49 +0600

    leetcode-180

--- mysql/leetcode-180.sql
+++ mysql/leetcode-180.sql
@@ -2,3 +2,14 @@ select DISTINCT l1.num as ConsecutiveNums
 from logs l1, logs l2, logs l3
 where l1.num = l2.num and l2.num = l3.num
 and l1.id = l2.id-1 and l2.id = l3.id-1
+
+#another solution using window function
+with temp as (
+    select num,
+    lead(num) over(ORDER BY id) as next_num,
+    lead(num, 2) over(order by id) as next_next_num
+    from logs
+)
+select DISTINCT num as ConsecutiveNums
+from temp
+where num = next_num and num = next_next_num
