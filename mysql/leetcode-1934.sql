835eb3ce7a61b1abff4e833b2e4ce4e9de91f49f
Sagor Chanda <ch.sagor27@gmail.com>
2026-08-21 12:29:32 +0600

    leetcode-1934

--- mysql/leetcode-1934.sql
+++ mysql/leetcode-1934.sql
@@ -10,3 +10,16 @@ from signups s left JOIN
 confirmations c on
 s.user_id = c.user_id
 GROUP BY s.user_id 
+
+
+select distinct s.user_id, 
+coalesce(round((select count(*)
+from confirmations c1
+where s.user_id = c1.user_id and action = 'confirmed')/
+(
+    select count(*)
+    from confirmations c2
+    where s.user_id = c2.user_id
+),2),0) as confirmation_rate
+from signups s left join confirmations c on
+s.user_id = c.user_id
