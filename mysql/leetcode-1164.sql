1deb300bf495183fbe7c9912d661054fbd22641f
Sagor Chanda <ch.sagor27@gmail.com>
2026-08-25 09:28:31 +0600

    leetcode-1164 (*new sol added)

--- mysql/leetcode-1164.sql
+++ mysql/leetcode-1164.sql
@@ -12,3 +12,23 @@ select p.product_id, (
 ) as price
 from (SELECT DISTINCT product_id
         from products) as p
+
+
+-- solution - 2 (took 1 hr almost)
+with base as (
+    select product_id, new_price, change_date
+    from products 
+    where (product_id, change_date) in
+    (
+        select product_id, max(change_date) as change_date
+        from products
+        where change_date <= '2019-08-16'
+        group by product_id
+    )
+)
+select p.product_id, coalesce(b.new_price, 10) as price
+from (
+    select distinct product_id
+    from products
+) p left join base b on
+p.product_id = b.product_id
