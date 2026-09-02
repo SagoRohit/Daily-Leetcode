d4e609e2f20c21fb8142307777b9ab28ca8f026b
Sagor Chanda <ch.sagor27@gmail.com>
2026-09-02 10:08:50 +0600

    leetcode-585 (another solution)

--- mysql/leetcode-585.sql
+++ mysql/leetcode-585.sql
@@ -15,3 +15,17 @@ tiv_2015 in
         GROUP BY tiv_2015
         having count(*)>1
     )
+
+
+ # Write your MySQL query statement below
+select round(sum(i.tiv_2016),2) as tiv_2016
+from insurance i
+where exists (select i2.pid 
+                from insurance i2 
+                where i.tiv_2015 = i2.tiv_2015 and
+                i.pid != i2.pid)
+and (i.lat, i.lon) not in
+( select i2.lat, i2.lon
+from insurance i2
+where i.pid != i2.pid
+)
