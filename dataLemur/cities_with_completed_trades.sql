e8e9b48f2ef6d93bf205240f4c9dc80164f49780
Sagor Chanda <ch.sagor27@gmail.com>
2026-09-05 00:12:33 +0600

    citis with comleted trades

---
+++ dataLemur/cities_with_completed_trades.sql
@@ -0,0 +1,6 @@
+select u.city as city, count(t.order_id) as total_orders
+from users u left join trades t on 
+u.user_id = t.user_id
+where t.status = 'Completed'
+group by u.city 
+order by total_orders desc limit 3
