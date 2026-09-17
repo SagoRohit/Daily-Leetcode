f39ceb5aeeae61ca054b5131cee6e1d491a377aa
Sagor Chanda <ch.sagor27@gmail.com>
2026-09-17 13:01:51 +0600

    stupid question of median

---
+++ dataLemur/search_median.sql
@@ -0,0 +1,10 @@
+with base as (
+  select searches
+  from search_frequency
+  group by searches,
+  generate_series(1, num_users))
+
+select 
+  round(percentile_cont(0.5) within group (
+    order by searches):: decimal ,1) as median
+    from base
