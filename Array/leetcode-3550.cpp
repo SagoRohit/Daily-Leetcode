20fbf14ce189e5393a29201fcbd61a58d326ce2d
Sagor Chanda <ch.sagor27@gmail.com>
2026-09-24 22:17:38 +0600

    leetcode-3550

---
+++ Array/leetcode-3550.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int digsum(int n) {
+        int sum = 0;
+        while(n) {
+            sum += n%10;
+            n/=10;
+        }
+        return sum;
+    }
+    int smallestIndex(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++) {
+            if(digsum(nums[i]) == i)
+                return i;
+        }
+        return -1;
+    }
+};
